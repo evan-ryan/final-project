b18e30cde1d18fe887ecd7f926967135e80b3bc8
evan-ryan <ewr1010@wildcats.unh.edu>
2021-11-28 23:04:58 -0500

    getting URLs for popular tweets

--- twitter_data.py
+++ twitter_data.py
@@ -13,8 +13,23 @@ auth = tweepy.OAuthHandler(api_key, api_secret)
 auth.set_access_token(access_token, access_secret)
 api = tweepy.API(auth)
 tweet_count = 1
-keyword_search = tweepy.Cursor(api.search_tweets, q="New York Knicks", tweet_mode="extended").items(tweet_count)
+keyword_search = tweepy.Cursor(api.search_tweets, q="New York Knicks", tweet_mode="extended", result_type="popular").items(tweet_count)
 cursor = keyword_search
+
+class UrlMaker():
+
+
+    def __init__(self, id_list):
+        self.id_list = id_list
+
+    def create(self):
+        url_list = []
+        for item in self.id_list:
+            url_list.append("https://twitter.com/twitter/statuses/" + str(item))
+        return url_list
+
+
+
 
 
 tweets = []
@@ -22,17 +37,18 @@ author = []
 retweets = []
 likes = []
 time = []
+urls = []
+ids = []
 
 for i in cursor:
     # print(dir(i))
     tweets.append(i.full_text)
     likes.append(i.favorite_count)
     time.append(i.created_at)
+    ids.append(i.id)
 
 
 
 
-df = pd.DataFrame({'tweets':tweets,'likes':likes,'time':time})
-
-print(df)
-print("this should only be in E")
+url_list = UrlMaker(ids)
+print(url_list.create())
